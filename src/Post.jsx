4bed3da9f2220127734378f11f66790cc829310f
Nema West <nemawest@Nemas-MacBook-Air.local>
2024-04-01 14:50:19 -0400

    Used state  to manage likes in Post component

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+
+import Comment from './comment';
 function Post(){
+  function handleLike(){
+    console.log('Liked!');
+    }
   return(
     <div>
       <h3>Test Post</h3>
       <p>This is a test post we'll edit later!</p>
       <p>Likes: 30</p>
       <p>Comments:</p>
+      <Comment content="This is a test comment!" />
+      <Comment content="This is another test comment!" />
+      <Comment content="This is yet another test comment!" />
     </div>
   )
 }
